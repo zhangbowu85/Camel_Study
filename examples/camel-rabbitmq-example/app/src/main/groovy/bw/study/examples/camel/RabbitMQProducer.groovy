ed68dd03e437df492a0aca9a9193eaba5886ca2d
bo-wu-zhang <bo-wu.zhang@hpe.com>
2021-05-11 06:05:27 +0300

    add a producer

--- examples/camel-rabbitmq-example/app/src/main/groovy/bw/study/examples/camel/RabbitMQProducer.groovy
+++ examples/camel-rabbitmq-example/app/src/main/groovy/bw/study/examples/camel/RabbitMQProducer.groovy
@@ -3,19 +3,19 @@
  */
 package bw.study.examples.camel
 
-import groovy.util.logging.Log4j2
+
 import groovy.util.logging.Slf4j
 import org.apache.camel.CamelContext
 import org.apache.camel.Exchange
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.impl.DefaultCamelContext
-import org.apache.camel.spi.ShutdownStrategy
+
+import java.time.Instant
 
 @Slf4j
-class RabbitMQConsumer {
+class RabbitMQProducer {
 
     static void main(String[] args) {
-        int count =1
         CamelContext camel = new DefaultCamelContext()
         camel.getShutdownStrategy().tap {
             it.setShutdownRoutesInReverseOrder(true)
@@ -25,9 +25,13 @@ class RabbitMQConsumer {
         camel.getGlobalOptions().put(Exchange.LOG_EIP_NAME, "bw.study.examples");
         RouteBuilder.addRoutes(camel, {
             it
-            .from("rabbitmq:mytest?addresses=localhost:5672&username=guest&password=guest")
-            .routeId('rabbitmq.client.consumer')
+            .from("timer:sendMessage?period=60000&repeatCount=-1")
+            .routeId('rabbitmq.client.producer')
+            .process {
+                it.in.body = 'Test Message ' + Instant.now()
+            }
             .to("log:bw.study.examples?showAll=true&multiline=true")
+            .to("rabbitmq:mytest?addresses=localhost:5672&username=guest&password=guest")
         })
         camel.addShutdownHook {
             sleep(60000)
